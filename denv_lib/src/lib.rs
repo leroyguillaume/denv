dc1d6800750477342e9834574c2af35347cfca6e
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2022-07-01 19:05:24 +0200

    test(util/downloader): use fr.archive.ubuntu.com for http tests
    refactor: add map_debug_err and debug_err macros
    refactor(util/downloader): use map_debug_err macro instead of map_err

--- denv_lib/src/lib.rs
+++ denv_lib/src/lib.rs
@@ -1,2 +1,33 @@
+macro_rules! debug_err {
+    ($result:expr) => {
+        if log::log_enabled!(log::Level::Debug) {
+            match $result {
+                Ok(res) => Ok(res),
+                Err(err) => {
+                    log::debug!("{}", err);
+                    Err(err)
+                }
+            }
+        } else {
+            $result
+        }
+    };
+}
+pub(crate) use debug_err;
+
+macro_rules! map_debug_err {
+    ($result:expr, $map:expr) => {
+        match $result {
+            Ok(res) => Ok(res),
+            Err(err) => {
+                let err = $map(err);
+                log::debug!("{}", err);
+                Err(err)
+            }
+        }
+    };
+}
+pub(crate) use map_debug_err;
+
 pub mod cfg;
 pub mod util;
