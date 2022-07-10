2c896cdf587cc420045075b8cd77a340f3dfb187
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2022-07-10 15:53:08 +0200

    feat(denv_lib/Fs): add create_bin_file fn
    refactor(denv_lib/Fs): remove ensure_too_dir fn

--- denv_lib/src/util/fs.rs
+++ denv_lib/src/util/fs.rs
@@ -5,10 +5,17 @@ use std::{
     path::{Path, PathBuf},
 };
 
-pub trait Fs {
-    fn create_tmp_file(&self, filename: &str) -> io::Result<File>;
+macro_rules! trace_open_file_w {
+    ($filepath:expr) => {{
+        trace!("Opening {} in write mode", $filepath.display());
+        OpenOptions::new().create(true).write(true).open($filepath)
+    }};
+}
 
-    fn ensure_tool_dir(&self, name: &str, version: &str) -> io::Result<PathBuf>;
+pub trait Fs {
+    fn create_bin_file(&self, tool_name: &str, version: &str) -> io::Result<File>;
+
+    fn create_tmp_file(&self, filename: &str) -> io::Result<File>;
 
     fn root_dirpath(&self) -> &Path;
 
@@ -30,27 +37,23 @@ impl DefaultFs {
 }
 
 impl Fs for DefaultFs {
-    fn create_tmp_file(&self, filename: &str) -> io::Result<File> {
-        let filepath = self.tmp_dirpath.join(filename);
-        trace!("Opening {} in write mode", filepath.display());
-        OpenOptions::new().create(true).write(true).open(filepath)
+    fn create_bin_file(&self, tool_name: &str, version: &str) -> io::Result<File> {
+        let dirpath = self
+            .root_dirpath
+            .join("tools")
+            .join(tool_name)
+            .join(version);
+        if dirpath.is_dir() {
+            debug!("Direction {} already exists", dirpath.display());
+        } else {
+            debug!("Creation directory {}", dirpath.display());
+            debug_err!(create_dir_all(&dirpath))?;
+        }
+        trace_open_file_w!(dirpath.join(tool_name))
     }
 
-    fn ensure_tool_dir(&self, name: &str, version: &str) -> io::Result<PathBuf> {
-        let path = self.root_dirpath.join(name).join(version);
-        if path.is_dir() {
-            debug!("Directory {} already exists", path.display());
-            Ok(path)
-        } else {
-            debug!("Creating directory {}", path.display());
-            match create_dir_all(&path) {
-                Ok(()) => Ok(path),
-                Err(err) => {
-                    debug!("Unable to create directory: {}", err);
-                    Err(err)
-                }
-            }
-        }
+    fn create_tmp_file(&self, filename: &str) -> io::Result<File> {
+        trace_open_file_w!(self.tmp_dirpath.join(filename))
     }
 
     fn root_dirpath(&self) -> &Path {
@@ -65,14 +68,22 @@ impl Fs for DefaultFs {
 #[cfg(test)]
 #[derive(Default)]
 pub struct StubFs {
+    create_bin_file_fn: Option<Box<CreateBinFileFn>>,
     create_tmp_file_fn: Option<Box<CreateTmpFileFn>>,
-    ensure_tool_dir_fn: Option<Box<EnsureToolDirFn>>,
 }
 
 #[cfg(test)]
 impl StubFs {
     pub fn new() -> Self {
         Self::default()
+    }
+
+    pub fn with_create_bin_file_fn<F: Fn(&str, &str) -> io::Result<File> + 'static>(
+        mut self,
+        create_bin_file_fn: F,
+    ) -> Self {
+        self.create_bin_file_fn = Some(Box::new(create_bin_file_fn));
+        self
     }
 
     pub fn with_create_tmp_file_fn<F: Fn(&str) -> io::Result<File> + 'static>(
@@ -82,28 +93,20 @@ impl StubFs {
         self.create_tmp_file_fn = Some(Box::new(create_tmp_file_fn));
         self
     }
-
-    pub fn with_ensure_tool_dir_fn<F: Fn(&str, &str) -> io::Result<PathBuf> + 'static>(
-        mut self,
-        ensure_tool_dir_fn: F,
-    ) -> Self {
-        self.ensure_tool_dir_fn = Some(Box::new(ensure_tool_dir_fn));
-        self
-    }
 }
 
 #[cfg(test)]
 impl Fs for StubFs {
-    fn create_tmp_file(&self, filename: &str) -> io::Result<File> {
-        match &self.create_tmp_file_fn {
-            Some(create_tmp_file_fn) => create_tmp_file_fn(filename),
+    fn create_bin_file(&self, tool_name: &str, version: &str) -> io::Result<File> {
+        match &self.create_bin_file_fn {
+            Some(create_bin_file_fn) => create_bin_file_fn(tool_name, version),
             None => unimplemented!(),
         }
     }
 
-    fn ensure_tool_dir(&self, name: &str, version: &str) -> io::Result<PathBuf> {
-        match &self.ensure_tool_dir_fn {
-            Some(ensure_tool_dir_fn) => ensure_tool_dir_fn(name, version),
+    fn create_tmp_file(&self, filename: &str) -> io::Result<File> {
+        match &self.create_tmp_file_fn {
+            Some(create_tmp_file_fn) => create_tmp_file_fn(filename),
             None => unimplemented!(),
         }
     }
@@ -118,10 +121,10 @@ impl Fs for StubFs {
 }
 
 #[cfg(test)]
-type CreateTmpFileFn = dyn Fn(&str) -> io::Result<File>;
+type CreateBinFileFn = dyn Fn(&str, &str) -> io::Result<File>;
 
 #[cfg(test)]
-type EnsureToolDirFn = dyn Fn(&str, &str) -> io::Result<PathBuf>;
+type CreateTmpFileFn = dyn Fn(&str) -> io::Result<File>;
 
 #[cfg(test)]
 mod test {
@@ -142,6 +145,30 @@ mod test {
                 let fs = DefaultFs::new(&root_dirpath, &tmp_dirpath);
                 assert_eq!(fs.root_dirpath(), root_dirpath);
                 assert_eq!(fs.tmp_dirpath(), tmp_dirpath);
+            }
+        }
+
+        mod create_bin_file {
+            use super::*;
+
+            #[test]
+            fn should_return_err() {
+                let root_dirpath = tempdir().unwrap().into_path().join("root");
+                let tmp_dirpath = tempdir().unwrap().into_path();
+                let fs = DefaultFs::new(&root_dirpath, &tmp_dirpath);
+                write(&root_dirpath, "").unwrap();
+                if fs.create_bin_file("terraform", "1.2.3").is_ok() {
+                    panic!("should fail");
+                }
+            }
+
+            #[test]
+            fn should_return_file() {
+                let root_dirpath = tempdir().unwrap().into_path();
+                let tmp_dirpath = tempdir().unwrap().into_path();
+                let fs = DefaultFs::new(&root_dirpath, &tmp_dirpath);
+                let mut file = fs.create_bin_file("terraform", "1.2.3").unwrap();
+                write!(file, "test").unwrap();
             }
         }
 
@@ -166,51 +193,6 @@ mod test {
                 let fs = DefaultFs::new(&root_dirpath, &tmp_dirpath);
                 let mut file = fs.create_tmp_file("terraform-1.2.3.zip").unwrap();
                 write!(file, "test").unwrap();
-            }
-        }
-
-        mod ensure_tool_dir {
-            use super::*;
-
-            #[test]
-            fn should_return_err() {
-                let name = "terraform";
-                let version = "1.2.3";
-                let root_dirpath = tempdir().unwrap().into_path();
-                let tmp_dirpath = tempdir().unwrap().into_path();
-                let path = root_dirpath.join(name).join(version);
-                let fs = DefaultFs::new(&root_dirpath, &tmp_dirpath);
-                create_dir_all(path.parent().unwrap()).unwrap();
-                write(&path, "").unwrap();
-                if fs.ensure_tool_dir(name, version).is_ok() {
-                    panic!("should fail");
-                }
-            }
-
-            #[test]
-            fn should_do_nothing() {
-                let name = "terraform";
-                let version = "1.2.3";
-                let root_dirpath = tempdir().unwrap().into_path();
-                let tmp_dirpath = tempdir().unwrap().into_path();
-                let expected = root_dirpath.join(name).join(version);
-                let fs = DefaultFs::new(&root_dirpath, &tmp_dirpath);
-                create_dir_all(&expected).unwrap();
-                let path = fs.ensure_tool_dir(name, version).unwrap();
-                assert_eq!(path, expected);
-            }
-
-            #[test]
-            fn should_create_dir() {
-                let name = "terraform";
-                let version = "1.2.3";
-                let root_dirpath = tempdir().unwrap().into_path();
-                let tmp_dirpath = tempdir().unwrap().into_path();
-                let expected = root_dirpath.join(name).join(version);
-                let fs = DefaultFs::new(&root_dirpath, &tmp_dirpath);
-                let path = fs.ensure_tool_dir(name, version).unwrap();
-                assert!(expected.is_dir());
-                assert_eq!(path, expected);
             }
         }
     }
