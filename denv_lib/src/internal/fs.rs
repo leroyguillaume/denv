4460fe30a7fe800b899f571e92cefe070e4f41b3
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2022-07-22 22:28:16 +0200

    feat(denv_lib/Fs): add create_env_file fn
    refactor(denv_lib/Fs): rename dir_id args into env_id

--- denv_lib/src/internal/fs.rs
+++ denv_lib/src/internal/fs.rs
@@ -19,10 +19,10 @@ macro_rules! ensure_dir {
 }
 
 macro_rules! env_dirpath {
-    ($denv_dirpath:expr, $dir_id:expr) => {
+    ($denv_dirpath:expr, $env_id:expr) => {
         $denv_dirpath
             .join(ENVIRONMENTS_DIRNAME)
-            .join(Path::new($dir_id))
+            .join(Path::new($env_id))
     };
 }
 
@@ -57,13 +57,16 @@ macro_rules! software_dirpath {
 
 const SOFTWARES_DIRNAME: &str = "softwares";
 const ENVIRONMENTS_DIRNAME: &str = "environments";
+const ENV_FILENAME: &str = "env";
 
 pub type Result<T> = std::result::Result<T, FileSystemError>;
 
 pub trait FileSystem {
     fn create_bin_file(&self, software: &dyn Software) -> Result<(PathBuf, File)>;
 
-    fn create_bin_symlink(&self, dir_id: &str, software: &dyn Software) -> Result<()>;
+    fn create_bin_symlink(&self, env_id: &str, software: &dyn Software) -> Result<()>;
+
+    fn create_env_file(&self, env_id: &str) -> Result<(PathBuf, File)>;
 
     fn create_tmp_file(&self, filename: &str) -> Result<(PathBuf, File)>;
 
@@ -112,6 +115,10 @@ impl FileSystem for DefaultFileSystem {
             .map_err(|err| FileSystemError::new(dest_filepath, err))
     }
 
+    fn create_env_file(&self, env_id: &str) -> Result<(PathBuf, File)> {
+        open_file!(env_dirpath!(self.denv_dirpath, env_id).join(ENV_FILENAME))
+    }
+
     fn create_tmp_file(&self, filename: &str) -> Result<(PathBuf, File)> {
         open_file!(self.tmp_dirpath.join(filename))
     }
@@ -136,6 +143,9 @@ type CreateBinFileFn = dyn Fn(&dyn Software) -> Result<(PathBuf, File)>;
 type CreateBinSymlinkFn = dyn Fn(&str, &dyn Software) -> Result<()>;
 
 #[cfg(test)]
+type CreateEnvFileFn = dyn Fn(&str) -> Result<(PathBuf, File)>;
+
+#[cfg(test)]
 type CreateTmpFileFn = dyn Fn(&str) -> Result<(PathBuf, File)>;
 
 #[cfg(test)]
@@ -146,6 +156,7 @@ type IsInstalledSoftwareFn = dyn Fn(&dyn Software) -> bool;
 pub struct StubFileSystem {
     create_bin_file_fn: Option<Box<CreateBinFileFn>>,
     create_bin_symlink_fn: Option<Box<CreateBinSymlinkFn>>,
+    create_env_file_fn: Option<Box<CreateEnvFileFn>>,
     create_tmp_file_fn: Option<Box<CreateTmpFileFn>>,
     is_installed_software_fn: Option<Box<IsInstalledSoftwareFn>>,
 }
@@ -169,6 +180,14 @@ impl StubFileSystem {
         create_bin_symlink_fn: F,
     ) -> Self {
         self.create_bin_symlink_fn = Some(Box::new(create_bin_symlink_fn));
+        self
+    }
+
+    pub fn with_create_env_file_fn<F: Fn(&str) -> Result<(PathBuf, File)> + 'static>(
+        mut self,
+        create_env_file_fn: F,
+    ) -> Self {
+        self.create_env_file_fn = Some(Box::new(create_env_file_fn));
         self
     }
 
@@ -198,9 +217,16 @@ impl FileSystem for StubFileSystem {
         }
     }
 
-    fn create_bin_symlink(&self, dir_id: &str, software: &dyn Software) -> Result<()> {
+    fn create_bin_symlink(&self, env_id: &str, software: &dyn Software) -> Result<()> {
         match &self.create_bin_symlink_fn {
-            Some(create_bin_symlink_fn) => create_bin_symlink_fn(dir_id, software),
+            Some(create_bin_symlink_fn) => create_bin_symlink_fn(env_id, software),
+            None => unimplemented!(),
+        }
+    }
+
+    fn create_env_file(&self, env_id: &str) -> Result<(PathBuf, File)> {
+        match &self.create_env_file_fn {
+            Some(create_bin_file_fn) => create_bin_file_fn(env_id),
             None => unimplemented!(),
         }
     }
@@ -357,6 +383,47 @@ mod test {
                 fs.create_bin_symlink(env_id, &software).unwrap();
                 assert!(dest_filepath.is_symlink());
                 assert_eq!(read_link(dest_filepath).unwrap(), src_filepath);
+            }
+        }
+
+        mod create_env_file {
+            use super::*;
+
+            #[test]
+            fn should_return_err() {
+                let env_id = "env_id";
+                let denv_dirpath = tempdir().unwrap().into_path().join("root");
+                let tmp_dirpath = tempdir().unwrap().into_path();
+                write(&denv_dirpath, "").unwrap();
+                let fs = DefaultFileSystem::new(denv_dirpath, tmp_dirpath);
+                if fs.create_env_file(env_id).is_ok() {
+                    panic!("should fail");
+                }
+            }
+
+            #[test]
+            fn should_return_filepath_and_file() {
+                let env_id = "env_id";
+                let denv_dirpath = tempdir().unwrap().into_path();
+                let tmp_dirpath = tempdir().unwrap().into_path();
+                let expected = env_dirpath!(denv_dirpath, env_id).join(ENV_FILENAME);
+                let fs = DefaultFileSystem::new(denv_dirpath, tmp_dirpath);
+                let (filepath, mut file) = fs.create_env_file(env_id).unwrap();
+                assert_eq!(filepath, expected);
+                write!(file, "test").unwrap();
+            }
+
+            #[test]
+            fn should_return_filepath_and_file_if_dir_does_not_exit() {
+                let env_id = "env_id";
+                let denv_dirpath = tempdir().unwrap().into_path();
+                let tmp_dirpath = tempdir().unwrap().into_path();
+                remove_dir_all(&denv_dirpath).unwrap();
+                let expected = env_dirpath!(denv_dirpath, env_id).join(ENV_FILENAME);
+                let fs = DefaultFileSystem::new(denv_dirpath, tmp_dirpath);
+                let (filepath, mut file) = fs.create_env_file(env_id).unwrap();
+                assert_eq!(filepath, expected);
+                write!(file, "test").unwrap();
             }
         }
 
